bbc909a2b0b83ca335e485f8b9f42516e540158f
shmen <shmen1994@gmail.com>
2017-07-31 16:46:51 +0800

    升级到webpack2  并加入vue

---
+++ src/index.js
@@ -0,0 +1,1 @@
+require('./style/style.css')
--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,103 @@
-var path = require('path');
-var webpack = require('webpack');
-var glob = require('glob')
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const glob = require('glob')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanPlugin = require('clean-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const extractSass = new ExtractTextPlugin({
+    filename: 'css/[name][hash].css',
+    disable: process.env.NODE_ENV === 'development'
+})
+
 var entries = getEntries('src/pages/**/index.js')
 var pages = getEntries('src/pages/**/*.html')
 var pagesArr = Object.keys(pages)
-var config = {
-    entry:entries,
+
+let config = {
+    entry: entries,
     output: {
-        path: path.join(__dirname, 'dist'), //输出目录的配置，模板、样式、脚本、图片等资源的路径配置都相对于它
-        publicPath: '/dist/', //模板、样式、脚本、图片等资源对应的server上的路径
-        filename: 'js/[name].js', //每个页面对应的主js的生成配置
-        chunkFilename: 'js/[id].chunk.js' //chunk生成的配置
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/dist/',
+        filename: 'js/[name][hash].js',
+        chunkFilename: 'js/[id].chunk.js'
     },
     module: {
-        loaders: [ //加载器，关于各个加载器的参数配置，可自行搜索之。
-            {
-                test: /\.css$/,
-                //配置css的抽取器、加载器。'-loader'可以省去
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            }, {
-                test: /\.less$/,
-                //配置less的抽取器、加载器。中间!有必要解释一下，
-                //根据从右到左的顺序依次调用less、css加载器，前一个的输出是后一个的输入
-                //你也可以开发自己的loader哟。有关loader的写法可自行谷歌之。
-                loader: ExtractTextPlugin.extract('css!less')
-            }, {
-                //html模板加载器，可以处理引用的静态资源，默认配置参数attrs=img:src，处理图片的src引用的资源
-                //比如你配置，attrs=img:src img:data-src就可以一并处理data-src引用的资源了，就像下面这样
-                test: /\.html$/,
-                loader: "html?attrs=img:src img:data-src"
-            }, {
-                //文件加载器，处理文件静态资源
-                test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: 'file-loader?name=./fonts/[name].[ext]'
-            }, {
-                //图片加载器，雷同file-loader，更适合图片，可以将较小的图片转成base64，减少http请求
-                //如下配置，将小于8192byte的图片转成base64码
-                test: /\.(png|jpg|gif)$/,
-                loader: 'url-loader?limit=8192&name=./img/[hash].[ext]'
+        rules: [{
+            test:/\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {
+                    css:extractSass.extract({
+                        use: [
+                            {loader: 'css-loader'},
+                            {loader:'sass-loader'}
+                        ],
+                        fallback: 'vue-style-loader'
+                    })
+                }
             }
-        ]
+        }, {
+            test: /\.s?css$/,
+            loader:extractSass.extract({
+                use: [{
+                    loader: 'css-loader'
+                }],
+                fallback: 'style-loader'
+            })
+        },{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        },{
+            test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+            loader: 'file-loader?name=fonts/[name].[hash].[ext]'
+        },{
+            test: /\.(png|jpg|gif|svg)$/,
+            loader: 'file-loader?limit=2048&name=imgs/[name].[hash].[ext]'
+        }]
     },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue': 'vue/dist/vue.esm.js'
+        }
+    },
+    devServer: {
+        contentBase: './',
+        host: 'localhost',
+        open:true,
+        openPage:'./dist/pages/index.html',
+        historyApiFallback: true,
+        noInfo: true,
+        port: 8080, //默认8080
+        inline: true //可以监控js变化
+    },
+    performance: {
+        hints: false
+    },
+    devtool: '#cheap-module-eval-source-map',
     plugins: [
         new webpack.ProvidePlugin({ //加载jq
             $: 'jquery'
         }),
+        new CleanPlugin(['dist']),
         new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendors', // 将公共模块提取，生成名为`vendors`的chunk
+            name: 'commons', // 将公共模块提取，生成名为`vendors`的chunk
             chunks: pagesArr, //提取哪些模块共有的部分
             minChunks: 3 // 提取至少3个模块共有的部分
         }),
-        new ExtractTextPlugin('css/[name].css'), //单独使用link标签加载css并设置路径，相对于output配置中的publickPath
-        new CleanWebpackPlugin('dist'),
-        new webpack.HotModuleReplacementPlugin() //热加载
-    ],
-    //使用webpack-dev-server，提高开发效率
-    devServer: {
-        contentBase: './',
-        host: 'localhost',
-        port: 9090, //默认8080
-        inline: true, //可以监控js变化
-        hot: true, //热启动
-    }
-};
-
-
-Object.keys(entries).forEach(function(name,index){
+        extractSass
+    ]
+}
+Object.keys(entries).forEach(function(name, index) {
     var template = pages[pagesArr[index]]
     var plugin = new HtmlWebpackPlugin({
         favicon: './favicon.png', //favicon路径，通过webpack引入同时可以生成hash值
-        filename: 'pages/'+name+'.html', //生成的html存放路径，相对于path
+        filename: 'pages/' + name + '.html', //生成的html存放路径，相对于path
         template: template, //html模板路径
         inject: true, //js插入的位置，true/'head'/'body'/false
         hash: true, //为静态资源生成hash值
-        chunks: ['vendors', name], //需要引入的chunk，不配置就会引入所有页面的资源
+        chunks: ['commons', name], //需要引入的chunk，不配置就会引入所有页面的资源
         minify: { //压缩HTML文件
             removeComments: true, //移除HTML中的注释
             collapseWhitespace: false //删除空白符与换行符
@@ -86,17 +107,17 @@ Object.keys(entries).forEach(function(name,index){
 })
 module.exports = config
 // 获取指定路径下的入口文件
-function getEntries (globPath) {
-     var files = glob.sync(globPath),
-       entries = {};
+function getEntries(globPath) {
+    var files = glob.sync(globPath),
+        entries = {};
 
-     files.forEach(function(filepath) {
-         // 取倒数第二层(view下面的文件夹)做包名
-         var split = filepath.split('/');
-         var name = split[split.length - 2];
+    files.forEach(function(filepath) {
+        // 取倒数第二层(view下面的文件夹)做包名
+        var split = filepath.split('/');
+        var name = split[split.length - 2];
 
-         entries[name] = './' + filepath;
-     });
+        entries[name] = './' + filepath;
+    });
 
-     return entries;
+    return entries;
 }
